d6d7cf0d1ff127bc4bfa218abd9eed32d29240d2
wilfredcloud <wilfredcloudspace@gmail.com>
2023-04-12 09:01:37 +0100

    Add cors header to resolve cors issue

---
+++ user_api/cors.header.php
@@ -0,0 +1,5 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Method: *');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Headers: *');
